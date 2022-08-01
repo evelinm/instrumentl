9a20eee5b12ae38edc43668801fa78925865354e
evelinm <mandamurillo910@gmail.com>
2022-08-01 10:24:01 -0700

    add api calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from 'react';
+import axios from "axios";
+
+const baseUrl = "https://api.thecatapi.com/v1/images/search";
+// const voteUrl = "https://api.thecatapi.com/v1/votes?api_key=a5efcafc-7a16-4bb8-bf77-fd8c69993137"
+const breedUrl = "https://api.thecatapi.com/v1/breeds?api_key=a5efcafc-7a16-4bb8-bf77-fd8c69993137";
 
 function App() {
+   const [data,setData] = useState([])
+   const [breed,setBreed] = useState([])
+
+
+   useEffect(()=> {
+     const fetchBreed = async () => {
+       const result = await axios.get(breedUrl);
+       setBreed(result.data)
+     }
+
+     const fetchData = async () => {
+       const result = await axios.get(baseUrl,
+        {
+          params: {
+            breed_id: breed
+
+          },
+        })
+       setData(result.data)
+     }
+
+     fetchBreed()
+  
+     fetchData()
+
+
+   },[])
+
+
+  console.log(data)
+  console.log("this is the breed",breed)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
   );
 }
